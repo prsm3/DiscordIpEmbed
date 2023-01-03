3e9f049410872b16597d107b3508cca5c997fce0
Wilhelm <111464533+prsm3@users.noreply.github.com>
2023-01-03 15:38:56 +0100

    Update to description and language

--- discordipsender.py
+++ discordipsender.py
@@ -1,28 +1,38 @@
+#imports the necessary libraries
 import requests
 import json
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+#defines a function for retrieving your public IP address
 def get():
+    #the endpoint from where the IP address is retrieved
     endpoint = "https://ipinfo.io/json"
     response = requests.get(endpoint, verify = True)
 
-    if response.status_code !=200:
-        return "Status: ", response.status_code, "Fehler bei der Anfrage."
+    #checking if the request has an error
+    if response.status_code != 200:
+        return "Status: ", response.status_code, " Error while retrieving the IP address."
         exit()
 
     data = response.json()
 
     return data["ip"]
 
-ipretreived = get()
+#forwards the returned ip to the variable "ipretrieved"
+ipretrieved = get()
 
-#paste your Webhook URL in the ""
+#paste your Discord Webhook URL in the ""
 webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1055569104905453589/NecAx0SnoVKl5HnuhsJAs0mCVFHiSlRBLyKD8xMy2oIVixbMvAQK8uN2XhY3-JC_qxCB")
 
 #color in HEX format "" or in RGB without the ""
-embed0 = DiscordEmbed(title="Die IP-Adresse", description=ipretreived, color="ffc0cb")
+#examples:
+#red = "ff0000"
+#blue = "0000ff"
+#pink = "ffc0cb"
+embed0 = DiscordEmbed(title="the IP address: ", description=ipretrieved, color="ffc0cb")
 
 #adds the embed to the Weebhook
 webhook.add_embed(embed0)
 
+#executes the webhook
 response = webhook.execute()
